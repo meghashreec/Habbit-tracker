a9006634187d7e1a02d20e6d98a4a73cb070d9f9
Megha <meghashekar296@gmail.com>
2026-03-08 18:10:55 +0530

    Initial habit tracker React project

---
+++ frontend/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1>Habit Tracker</h1>
+      <p>Welcome to your Habit Tracker!</p>
+      <ul>
+        <li>Drink ABC</li>
+        <li>Exercise</li>
+        <li>Meditation</li>
+      </ul>
+    </>
+  );
+}
+
+export default App;
